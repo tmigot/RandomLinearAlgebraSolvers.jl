90bab86fabcdd6d6592e15ddd7af56bc3c8f6263
tmigot <tangi.migot@gmail.com>
2021-11-23 11:19:46 -0500

    add doc

--- src/RandomKrylov.jl
+++ src/RandomKrylov.jl
@@ -47,6 +47,14 @@ end
 
 export RLAStopping
 
+"""
+    stp = RLAStopping(A, b::S; n_listofstates::Int = 0, kwargs...)
+
+Creator of a `LAStopping` tailored for this package.
+The problem, stp.pb, is a `Stopping.LinearSystem` if `A` is dense, and a `LLSModels.LLSModel` otherwise.
+The state allocates space for the residual, `stp.current_state.res`, of length `|b|`.
+This stopping uses the infinity norm of `stp.current_state.res` to declare optimality.
+"""
 function RLAStopping(A, b::S; n_listofstates::Int = 0, kwargs...) where {S}
   T = eltype(S)
   sparse = issparse(A)
